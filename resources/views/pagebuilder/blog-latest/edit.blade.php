51c784c49f2e3dbf2b726601799a68e39ff64722
Kieran Metcalfe <kieran@ascent-creative.co.uk>
2022-08-30 20:49:56 +0100

    PgaeBuilder blocks

---
+++ resources/views/pagebuilder/blog-latest/edit.blade.php
@@ -0,0 +1,11 @@
+
+
+<div class="flex flex-center" style="width: 100%">
+    <H2 class="text-center" style="width: 100%; padding: 10px;">
+        <x-forms-fields-wysiwyg :value="$value->title ?? ''" placeholder="Latest Blog Posts" label="" wrapper="none" name="{{$name}}[title]" toolbar="basic"/>
+    </H2>
+</div>
+
+<div class="container">
+    <x-cms-form-input type="text" label="Headline" :value="$value->headline ?? ''" name="{{ $name }}[headline]" />
+</div>
